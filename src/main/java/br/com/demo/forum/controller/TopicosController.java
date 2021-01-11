17246ee910ac9c8e5d60e5bc99c472fd68e03a8b
Diego Romario <diego.romario@outlook.com>
2021-01-11 16:02:49 -0300

    Adicionando metodo para incluír topicos

--- src/main/java/br/com/demo/forum/controller/TopicosController.java
+++ src/main/java/br/com/demo/forum/controller/TopicosController.java
@@ -1,31 +1,51 @@
 package br.com.demo.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import br.com.demo.forum.controller.dto.TopicoDto;
-import br.com.demo.forum.modelo.Topico;
 import br.com.demo.forum.repository.TopicoRepository;
+import br.com.demo.forum.controller.form.TopicoForm;
+import br.com.demo.forum.modelo.Topico;
+import br.com.demo.forum.repository.CursoRepository;
+import br.com.demo.forum.controller.dto.TopicoDto;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
-
-    @Autowired
-    private TopicoRepository topicoRepository;
-
-    @RequestMapping("/topicos")
-    public List<TopicoDto> lista(String nomeCurso) {
-        System.out.println(nomeCurso);
-        if (nomeCurso == null) {
-            List<Topico> topicos = topicoRepository.findAll();
-            return TopicoDto.converter(topicos);
-        } else {
-            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
-            return TopicoDto.converter(topicos);
-        }
-    }
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@GetMapping
+	public List<TopicoDto> lista(String nomeCurso) {
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
 
 }
