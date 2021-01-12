f3725004897857b915a9651dc7a5b7dd8aece49e
Diego Romario <diego.romario@outlook.com>
2021-01-12 15:31:41 -0300

    Adicionando regras para acesso a URLs publicas

--- src/main/java/br/com/demo/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/demo/forum/config/security/SecurityConfigurations.java
@@ -1,11 +1,34 @@
 package br.com.demo.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    // Configurações de autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    // Configurações de autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+
+    // Configurações de recursos estáticos (css, imagem, js etc)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
 
 }
