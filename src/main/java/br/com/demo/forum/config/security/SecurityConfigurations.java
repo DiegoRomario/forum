895dd5ddaae1f3929e9d9792d821afe308903719
Diego Romario <diego.romario@outlook.com>
2021-01-14 10:45:13 -0300

    Adicionando Actuator ao projeto

--- src/main/java/br/com/demo/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/demo/forum/config/security/SecurityConfigurations.java
@@ -35,10 +35,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     // Configuracoes de autorizacao
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
-                .anyRequest().authenticated().and().csrf().disable().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll().antMatchers(HttpMethod.GET, "/topicos/*")
+                .permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll().anyRequest().authenticated().and().csrf()
+                .disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     // Configuracoes de recursos estaticos(js, css, imagens, etc.)
