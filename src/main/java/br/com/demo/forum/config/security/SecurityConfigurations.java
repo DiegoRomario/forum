446d4042ecf5a85d31401db8268ded2635205b3b
Diego Romario <diego.romario@outlook.com>
2021-01-14 15:15:25 -0300

    Adicionando controle de acesso por perfil para exclusão de topicos

--- src/main/java/br/com/demo/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/demo/forum/config/security/SecurityConfigurations.java
@@ -46,8 +46,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
-                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll().anyRequest().authenticated().and().csrf()
-                .disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll().antMatchers(HttpMethod.DELETE, "/topicos/*")
+                .hasRole("MODERADOR").anyRequest().authenticated().and().csrf().disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository),
                         UsernamePasswordAuthenticationFilter.class);
     }
