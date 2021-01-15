8f34927be31da8fadc8d9f8a6ffc61429d91eaca
Diego Romario <diego.romario@outlook.com>
2021-01-15 09:14:46 -0300

    Adicionando primeiros testes para repositorio de cursos

---
+++ src/test/java/br/com/demo/forum/repository/CursoRepositoryTeste.java
@@ -0,0 +1,35 @@
+package br.com.demo.forum.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.demo.forum.modelo.Curso;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CursoRepositoryTeste {
+
+    private static final String CURSO_EXISTENTE = "HTML 5";
+    private static final String CURSO_INEXISTENTE = "VISUAL BASIC";
+    @Autowired
+    private CursoRepository _repository;
+
+    @Test
+    public void dadoNomeDeUmCursoExistente_DeveBuscarDoBancoDeDados() {
+        Curso curso = _repository.findByNome(CURSO_EXISTENTE);
+        Assert.assertNotNull(curso);
+        Assert.assertEquals(CURSO_EXISTENTE, curso.getNome());
+    }
+
+    @Test
+    public void dadoNomeDeUmCursoInexistente_DeveRetornarNulo() {
+        Curso curso = _repository.findByNome(CURSO_INEXISTENTE);
+        Assert.assertNull(curso);
+    }
+
+
+}
